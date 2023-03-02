ba89bf359da917893c9b4aca31daaef7156f3fa2
Song Xu <song.xu@thoughtworks.com>
2023-03-02 15:38:27 +0800

    init repo

---
+++ src/test/java/com/yuzhongyangwang/messagemqdemo/MessageMqDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuzhongyangwang.messagemqdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageMqDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
