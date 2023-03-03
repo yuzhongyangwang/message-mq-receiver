e4ed0ab73517868c8bf5ff654bf149aca777ec8b
Song Xu <song.xu@thoughtworks.com>
2023-03-03 15:10:19 +0800

    add sent server to send message: hello word

--- src/main/java/com/yuzhongyangwang/messagemqdemo/MessageMqDemoApplication.java
+++ src/main/java/com/yuzhongyangwang/messagemqdemo/MessageMqDemoApplication.java
@@ -22,18 +22,6 @@ public class MessageMqDemoApplication {
 			channel.basicPublish("", QUEUE_NAME, null, message.getBytes(StandardCharsets.UTF_8));
 			System.out.println(" [x] Sent '" + message + "'");
 		}
-
-		Connection connection = factory.newConnection();
-		Channel channel = connection.createChannel();
-
-		channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-		System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
-
-		DeliverCallback deliverCallback = (consumerTag, delivery) -> {
-			String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
-			System.out.println(" [x] Received '" + message + "'");
-		};
-		channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> { });
 		SpringApplication.run(MessageMqDemoApplication.class, args);
 	}
 
